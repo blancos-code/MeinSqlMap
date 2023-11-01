f0776a5f288d85a79aba09ba20635223e6ca1f33
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2023-11-01 16:08:54 +0100

    feature: Lancement de sql map pour les url valides avec une interface gainée et champ de recherche a automatiquement inurl

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import threading
+
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO
 import requests
+import subprocess
+import shlex
 
 app = Flask(__name__)
 
@@ -7,6 +12,8 @@ GOOGLE_SEARCH_API_ENDPOINT = "https://www.googleapis.com/customsearch/v1"
 API_KEY = "AIzaSyCvl-lGhRJ12dYAnhAr1HgKdWmINWABus8"
 CX = "57447145afad04e1b"
 app.secret_key = 'super_secret'
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route("/")
@@ -43,15 +50,39 @@ def search():
 
 @app.route("/results")
 def results():
-    # Récupérer les résultats de la session
     results = session.get('results', [])
+    for index, result in enumerate(results):
+        if result['isValidForPentesting']:
+            handle_start_scan({'url': result['url'], 'index': index})
     return render_template("results.html", results=results)
 
 
 def isUrlValid(url):
     query = session.get('query', '')
 
-    return ('=' in url) or ("/"+query+"/" in url)
+    return ('=' in url) or ("/" + query + "/" in url)
+
+
+@socketio.on('start_scan')
+def handle_start_scan(data):
+    url = data['url']
+    index = data['index']
+    thread = threading.Thread(target=run_sqlmap, args=(url, index))
+    thread.start()
+
+
+def run_sqlmap(url, index):
+    command = f"python sql_map/sqlmap.py -u {url} --batch -dbs"
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    while True:
+        output = process.stdout.readline()
+        if not output and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+            socketio.emit('console_output', {'data': output.strip(), 'index': index})
+    rc = process.poll()
+    return rc
 
 
 
