43369945010d9d918516d1c0abdd4739356fa7a0
DyaZix-Code <blancosacha38@gmail.com>
2023-11-02 12:34:58 +0100

    feature: Chercher sur plusieurs dizaines de pages

--- controller/search_controller.py
+++ controller/search_controller.py
@@ -13,28 +13,38 @@ def search():
     if request.method == "POST":
         query = request.form.get("query")
         start = request.form.get("start")
+        page_count = request.form.get("page_count")
 
         if query:
-            params = {
-                "q": "inurl:" + query,
-                "key": API_KEY,
-                "cx": CX,
-                "start": start
-            }
-            response = requests.get(GOOGLE_SEARCH_API_ENDPOINT, params=params)
-            items = response.json().get("items", [])
-
             results = []
-            for item in items:
-                result = {'url': item.get('link', '')}
-                result['is_valid_for_pentesting'] = is_url_valid(result['url'])
-                results.append(result)
+
+            results = get_results_from_google(page_count, query, results, start)
 
             session['results'] = results
             session['query'] = query
             return redirect(url_for('results'))
     return render_template("search.html")
 
+
+def get_results_from_google(page_count, query, results, start):
+
+    for page in range(int(start), int(start + page_count)):
+        params = {
+            "q": "inurl:" + query,
+            "key": API_KEY,
+            "cx": CX,
+            "start": page
+        }
+
+        response = requests.get(GOOGLE_SEARCH_API_ENDPOINT, params=params)
+        items = response.json().get("items", [])
+        print(response.json())
+        for item in items:
+            result = {'url': item.get('link', '')}
+            result['is_valid_for_pentesting'] = is_url_valid(result['url'])
+            results.append(result)
+
+    return results
 
 def is_url_valid(url):
     query = session.get('query', '')
