1b1ffc42d5cb50a62fa0fc7c47a66dc3be6bf199
DyaZix-Code <blancosacha38@gmail.com>
2023-11-02 15:27:00 +0100

    feature: bootstrap 5.2.0 et page results avec des accordions

--- static/js/console.js
+++ static/js/console.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOM chargé !")
     var socket = io.connect('http://' + document.domain + ':' + location.port);
 
     socket.on('connect', function() {
@@ -8,18 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     socket.on('console_output', function(data) {
-        var index = data.index;
-        var url = data.url;
-        var output = data.data.replace(/\n/g, '<br>'); // Remplacez les sauts de ligne par des balises <br>
+    var index = data.index;
+    var url = data.url;
+    var output = data.data.replace(/\n/g, '<br>');
 
-        // Trouvez l'élément pour afficher la console de cette URL spécifique
-        var consoleElement = document.getElementById('console-' + index);
-        var urlElement = document.getElementById('url-' + index);
+    var a = document.createElement('a');
+    a.href = url;
 
-        if (urlElement) {
-            urlElement.innerHTML = url;
-            urlElement.href = url;
-        }
+    var domain = a.hostname;
+
+    var consoleElement = document.getElementById('console-' + index);
+    var urlElement = document.getElementById('url-' + index);
+
+    if (urlElement) {
+        urlElement.innerHTML = domain; // Affichez le nom de domaine au lieu de l'URL complète
+        urlElement.href = url;
+    }
+
         if (consoleElement) {
             var preElement = consoleElement.querySelector('pre');
             // Ajoutez la nouvelle ligne de sortie à la console
