751f99697b413c73046149265c0b531a446ee857
DyaZix-Code <blancosacha38@gmail.com>
2023-11-03 10:37:21 +0100

    fix: refractor sqlmap_service

--- controller/results_controller.py
+++ controller/results_controller.py
@@ -1,10 +1,7 @@
-import threading
-import subprocess
-import shlex
-
-from utils.console_socketio_service import display_to_console
 from flask import Blueprint, render_template, session
 from flask_socketio import SocketIO
+
+from utils.sqlmap_service import handle_start_scan
 
 results_blueprint = Blueprint('results', __name__)
 
@@ -16,29 +13,5 @@ def results():
     search_results = session.get('results', [])
     for index, result in enumerate(search_results):
         if result['is_valid_for_pentesting']:
-            handle_start_scan({'url': result['url'], 'index': index})
+            handle_start_scan({'url': result['url'], 'index': index}, socketio)
     return render_template("results.html", results=search_results)
-
-
-def handle_start_scan(data):
-    url = data['url']
-    index = data['index']
-    thread = threading.Thread(target=run_sqlmap, args=(url, index))
-    thread.start()
-
-
-def run_sqlmap(url, index):
-    command = f"py sql_map/sqlmap.py -u {url} --batch -dbs"
-    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-
-    socketio.emit('console_output', {'data': url + "\n", 'index': index})
-
-    while True:
-        output = process.stdout.readline()
-        if not output and process.poll() is not None:
-            break
-
-        display_to_console(output, socketio, url, index)
-
-    rc = process.poll()
-    return rc
---
+++ utils/sqlmap_service.py
@@ -0,0 +1,28 @@
+import shlex
+import subprocess
+import threading
+from utils.console_socketio_service import display_to_console
+
+
+def handle_start_scan(data, socketio):
+    url = data['url']
+    index = data['index']
+    thread = threading.Thread(target=run_sqlmap, args=(url, index, socketio))
+    thread.start()
+
+
+def run_sqlmap(url, index, socketio):
+    command = f"py sql_map/sqlmap.py -u {url} --batch -dbs"
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    socketio.emit('console_output', {'data': url + "\n", 'index': index})
+
+    while True:
+        output = process.stdout.readline()
+        if not output and process.poll() is not None:
+            break
+
+        display_to_console(output, socketio, url, index)
+
+    rc = process.poll()
+    return rc
