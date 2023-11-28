0d065fae53ac4ef0fc9a87177e22dc3d0626f2fb
Estelle JOYEUX BOUILLON <estelle.joyeuxbouillon@hardis-group.com>
2023-11-28 12:25:24 +0100

    edit style

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,8 @@
-
-
 const cursor = document.getElementById("cursor-custom"),
   radius = cursor.offsetHeight / 2;
 
 document.addEventListener("mousemove", (e) => {
   let top = e.clientY - radius,
     left = e.clientX - radius;
-  console.log("bip")
   cursor.style = `top: ${top}px; left: ${left}px`;
 });
