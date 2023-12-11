810cb5c643b2bb36a2a20a72390e9a48ab0d0ec3
Estelle JOYEUX BOUILLON <estelle.joyeuxbouillon@hardis-group.com>
2023-12-11 10:33:59 +0100

    add background hacker

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 const cursor = document.getElementById("cursor-custom"),
   radius = cursor.offsetHeight / 2;
 
@@ -8,6 +6,3 @@ document.addEventListener("mousemove", (e) => {
     left = e.clientX - radius;
   cursor.style = `top: ${top}px; left: ${left}px`;
 });
->>>>>>> be71133 (add background hacker)
-// import { MDCDataTable } from "@material/data-table";
-// const dataTable = new MDCDataTable(document.querySelector(".mdc-data-table"));
